64387cd72c84670c07c4cc27bd88cc5b6c6338cf
josephanandyerrakonda <167710543+josephanandyerrakonda@users.noreply.github.com>
2024-06-09 19:36:51 +0200

    doing project

--- src/test/java/data_management/DataStorageTest.java
+++ src/test/java/data_management/DataStorageTest.java
@@ -12,14 +12,27 @@ class DataStorageTest {
 
     @Test
     void testAddAndGetRecords() {
-        // TODO Perhaps you can implement a mock data reader to mock the test data?
+        DataReader reader = new MockDataReader();
         // DataReader reader
         DataStorage storage = new DataStorage(reader);
+
+        try {
+            reader.readData(storage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         storage.addPatientData(1, 100.0, "WhiteBloodCells", 1714376789050L);
         storage.addPatientData(1, 200.0, "WhiteBloodCells", 1714376789051L);
 
         List<PatientRecord> records = storage.getRecords(1, 1714376789050L, 1714376789051L);
         assertEquals(2, records.size()); // Check if two records are retrieved
         assertEquals(100.0, records.get(0).getMeasurementValue()); // Validate first record
+    }
+}   class MockDataReader implements DataReader {
+    @Override
+    public void readData(DataStorage dataStorage) {
+        // Add mock data to storage
+        dataStorage.addPatientData(1, 75.0, "HeartRate", 1714376789000L);
+        dataStorage.addPatientData(1, 120.0, "HeartRate", 1714376789001L);
     }
 }
